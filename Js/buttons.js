3854e819eba2f803152218e15b60e26ab63570c8
Nicolas Lavarello <lavarello.nicolas.des@gmail.com>
2023-03-13 14:37:39 -0300

    Proyecto Integrador Final

---
+++ Js/buttons.js
@@ -0,0 +1,11 @@
+/*Modificador de color de fondo*/
+function cambiar_colorfondo() {
+    let body = document.getElementsByTagName ("body") [0];
+    body.style.backgroundColor = "#f00";
+}
+
+/*Restaurador de color de fondo*/
+function restaurar_colorfondo() {
+    let body = document.getElementsByTagName ("body") [0];
+    body.style.backgroundColor = "hsl(212, 21%, 14%)";
+}
